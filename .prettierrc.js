ab3961ea4133f1d90728047aad7996c3d36934a0
huynhduyquy2001 <huynhduyquy2001@gmail.com>
2024-06-06 15:22:40 +0700

    Create global settings, collection Ads, Position

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,6 +3,6 @@ module.exports = {
   parser: 'typescript',
   semi: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'none',
   arrowParens: 'avoid',
 }
--- src/payload/collections/FeatureExchange/index.ts
+++ src/payload/collections/FeatureExchange/index.ts
@@ -1,30 +1,30 @@
 // src/collections/FeatureCoin.js
 
-import { CollectionConfig } from 'payload/types';
-import { isAdmin } from '../../access/isAdmin';
-import { anyone } from '../../access/anyone';
+import { CollectionConfig } from 'payload/types'
+import { isAdmin } from '../../access/isAdmin'
+import { anyone } from '../../access/anyone'
 
 const FeatureExchange: CollectionConfig = {
-    slug: 'feature-exchange',
-    access: {
-        create: isAdmin,
-        read: anyone,
-        readVersions: isAdmin,
-        update: isAdmin,
-        delete: isAdmin,
+  slug: 'feature-exchange',
+  access: {
+    create: isAdmin,
+    read: anyone,
+    readVersions: isAdmin,
+    update: isAdmin,
+    delete: isAdmin,
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
     },
-    fields: [
-        {
-            name: 'name',
-            type: 'text',
-            required: true,
-        },
-        {
-            name: 'rank',
-            type: 'number',
-            required: true,
-        },
-    ],
-};
+    {
+      name: 'rank',
+      type: 'number',
+      required: true,
+    },
+  ],
+}
 
-export default FeatureExchange;
+export default FeatureExchange
